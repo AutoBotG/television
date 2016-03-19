42f52d811a557cf3c5e810919f9629a5ee582fe8
AutoBotG <Girish.Pottigari@uk.tesco.com>
2016-03-19 13:15:36 +0000

    updating step definations

--- features/step_definitions/television_review_steps.rb
+++ features/step_definitions/television_review_steps.rb
@@ -20,3 +20,49 @@ Then(/^review result should provide the following information$/) do |table|
   end
   expect(assertion_messages).to eql []
 end
+
+Then(/^I should have the following sorting options$/) do |table|
+  assertion_messages = []
+  table.hashes.each do |row|
+    begin
+      expect(which.review.sort_dropdown).to have_option(row[:options])
+    rescue RSpec::Expectations::ExpectationNotMetError, Capybara::ElementNotFound => e
+      assertion_messages << (e.message + "#{row}")
+    end
+  end
+  expect(assertion_messages).to eql []
+end
+
+When(/^I sort the results by "([^"]*)"$/) do |option|
+  which.review.sort_dropdown.sort_by(option)
+end
+
+Then(/^the review results should be sorted by Price (\(low to high\)|\(high to low\))$/) do |option|
+  which.review.wait_until_loading_indicator_invisible
+  actual = which.review.results.map {|result| result.info[:price]}
+  expected = option == "(low to high)" ? actual.sort : actual.sort.reverse
+  expect(actual).to eql(expected)
+end
+
+When(/^I refine the review results by "([^"]*)"$/) do |arg|
+  pending
+end
+
+Then(/^the review results should be refined by "([^"]*)"$/) do |arg|
+  pending
+end
+
+Then(/^sort option "([^"]*)" should be disabled$/) do |option|
+  expect(which.review.sort_dropdown.disabled_option.text).to eql option
+end
+
+When(/^I refine the review results by screen size (.+)$/) do |option|
+  which.review.screen_filter.filter_by(option)
+end
+
+Then(/^the review results should be refined by screen size (.+)$/) do |option|
+  which.review.wait_until_loading_indicator_invisible
+  facts =  which.review.results.map {|result| result.info[:key_fact][/\d+/].to_i}
+  range = option.scan(/\d+/)
+  facts.each {|fact| expect(fact).to be_between(range[0].to_i, range[1].to_i)}
+end
